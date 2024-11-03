fbae9f48f95fea42c7f3d4f7312961b1b7d28884
Ariel Spencer <arielspencer.tech@gmail.com>
2024-11-02 23:52:20 -0300

    arithmetic and assignment operations

--- aula01.playground/Pages/operacoes-aritmeticas-e-atribuicao.xcplaygroundpage/Contents.swift
+++ aula01.playground/Pages/operacoes-aritmeticas-e-atribuicao.xcplaygroundpage/Contents.swift
@@ -1,7 +1,30 @@
-//: [Previous](@previous)
+10 + 2
+10 - 2
+8 * 2
+10 / 2
 
-import Foundation
+5 / 2
+5.0 / 2
+Double(5) / 2
 
-var greeting = "Hello, playground"
+5 % 2
+4 % 2
 
-//: [Next](@next)
+5 + 2 * 10
+(5 + 2) * 10
+
+var numero1 = 10
+var numero2 = 50
+var soma = numero1 + numero2 + 2
+print(soma)
+
+numero1 = numero1 + 2
+numero2 += 2
+numero1 -= 2
+numero1 *= 2
+numero2 /= 2
+
+var nome = "Ariel"
+var sobrenome = "Spencer"
+print(nome + " " + sobrenome)
+print("Meu sobrenome é: \(sobrenome)")
