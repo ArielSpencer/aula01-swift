99cf52dba1cd6fc378cc855636c65e6e8f51d6cc
Ariel Spencer <arielspencer.tech@gmail.com>
2024-11-02 22:59:25 -0300

    declare variables and data types

--- aula01.playground/Contents.swift
+++ aula01.playground/Contents.swift
@@ -1,2 +1,15 @@
-var greeting = "Hello, playground"
-print(greeting)
+var nome: String = "Ariel"
+let idade: Int = 28
+
+print("O meu nome é \(nome)")
+print(idade)
+
+var coeficienteRendimento = 9.5
+var possuiHistoricoReprovacao: Bool
+possuiHistoricoReprovacao = false
+
+var turnoEmQueEstuda: Character = "D"
+
+typealias Estudante = String
+var aluno1: Estudante = "João"
+var aluno2: Estudante = "Maria"
