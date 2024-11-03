82d330f0053e9f0064885287a3b5385e64fe1439
Ariel Spencer <arielspencer.tech@gmail.com>
2024-11-02 23:13:21 -0300

    show personal information

--- aula01.playground/Pages/ficha-simples.xcplaygroundpage/Contents.swift
+++ aula01.playground/Pages/ficha-simples.xcplaygroundpage/Contents.swift
@@ -1,7 +1,13 @@
-//: [Previous](@previous)
+var inicialDoNome: Character = "A"
+var nome: String = "Ariel"
+var sobrenome: String = "Spencer"
+var idade: Int = 28
+var altura: Double = 1.80
+var gostaDeSwift: Bool = true
 
-import Foundation
-
-var greeting = "Hello, playground"
-
-//: [Next](@next)
+print("Inicial do nome: \(inicialDoNome)")
+print("Nome: \(nome)")
+print("Sobrenome: \(sobrenome)")
+print("Idade: \(idade)")
+print("Altura: \(altura)")
+print("Gosta de Swift: \(gostaDeSwift)")
