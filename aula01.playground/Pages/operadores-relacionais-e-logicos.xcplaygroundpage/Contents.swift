decc80f3c8ca6fdcbab2c755c1906b386063231d
Ariel Spencer <arielspencer.tech@gmail.com>
2024-11-03 23:51:12 -0300

    feat: add relational and logical operators

--- aula01.playground/Pages/operadores-relacionais-e-logicos.xcplaygroundpage/Contents.swift
+++ aula01.playground/Pages/operadores-relacionais-e-logicos.xcplaygroundpage/Contents.swift
@@ -1,7 +1,21 @@
-//: [Previous](@previous)
+var pontuacao1 = 8.0
+var pontuacao2 = 10.0
 
-import Foundation
+pontuacao1 > pontuacao2
+pontuacao1 >= pontuacao2
+pontuacao1 < pontuacao2
+pontuacao1 <= pontuacao2
+pontuacao1 == pontuacao2
+pontuacao1 != pontuacao2
 
-var greeting = "Hello, playground"
+var usuario1Venceu = pontuacao1 > pontuacao2
+var usuario2Venceu = pontuacao1 < pontuacao2
+var empate = pontuacao1 == pontuacao2
 
-//: [Next](@next)
+var idade = 28
+var adulto = idade >= 18
+var possuiCnhValida = false
+var podeDirigir = adulto && possuiCnhValida
+var podeComprarCarro = adulto || possuiCnhValida
+
+!adulto
