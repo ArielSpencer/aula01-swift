021501cfb50d8a3d5c026231602b6f270b9267d8
Ariel Spencer <arielspencer.tech@gmail.com>
2024-11-04 22:37:11 -0300

    feat: add range of values ​​and conditionals with switch

--- aula01.playground/Pages/intervalo-de-valores-e-condicionais-com-switch.xcplaygroundpage/Contents.swift
+++ aula01.playground/Pages/intervalo-de-valores-e-condicionais-com-switch.xcplaygroundpage/Contents.swift
@@ -1,7 +1,26 @@
-//: [Previous](@previous)
+var intervaloFechado = 10...20
+var intervaloSemiAberto = 10..<20
 
-import Foundation
+var numero = 15
 
-var greeting = "Hello, playground"
+switch numero {
+case 0...10: print("Número está entre 0 e 10")
+case 10...20: print("Número está entre 10 e 20")
+default: print("Número é maior do que 20")
+}
 
-//: [Next](@next)
+var animal = "Gato"
+
+switch animal {
+case "Cachorro", "Gato": print("O animal é domestico")
+default: print("O animal é selvagemo")
+}
+
+switch numero {
+case _ where numero % 2 == 0: print("O número é par")
+case _ where numero % 2 != 0: print("O número é ímpar")
+default: break
+}
+
+var numeroAleatorio = Int.random(in: 1...100)
+print(numeroAleatorio)
