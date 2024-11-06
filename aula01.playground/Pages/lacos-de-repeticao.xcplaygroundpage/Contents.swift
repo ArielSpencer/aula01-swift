ddc1632e66bf5cd7f137f165da087dac1c47db2e
Ariel Spencer <arielspencer.tech@gmail.com>
2024-11-05 22:52:51 -0300

    feat: add break and continue

--- aula01.playground/Pages/lacos-de-repeticao.xcplaygroundpage/Contents.swift
+++ aula01.playground/Pages/lacos-de-repeticao.xcplaygroundpage/Contents.swift
@@ -1,16 +1,16 @@
-//with for:
+print("with for:")
 for _ in 1...3 {
     print("Eu amo Swift!")
 }
 
-//with while:
+print("with while:")
 var count = 1
 while count <= 3 {
     print("Eu amo Café!")
     count += 1
 }
 
-//with repeat while:
+print("with repeat while:")
 var i = 1
 repeat {
     print("Eu amo Programação!")
@@ -19,13 +19,13 @@ repeat {
 
 var soma = 0
 
-// e.g. sum:
+print("e.g. sum:")
 for i in 1...7 {
     soma += i
 }
 print(soma)
 
-// e.g. 2 sum:
+print("e.g. sum2:")
 var soma2 = 1
 var count2 = 0
 repeat {
@@ -34,3 +34,25 @@ repeat {
 } while(count2 < 5)
 
 print(soma2)
+
+print("with break:")
+var count3 = 1
+
+while count3 <= 10 {
+    if count3 == 5 {
+        break
+    }
+    print(count3)
+    count3 += 1
+}
+
+print("with continue:")
+var count4 = 0
+
+while count4 <= 10 {
+    count4 += 1
+    if count4 == 5 {
+        continue
+    }
+    print(count4)
+}
