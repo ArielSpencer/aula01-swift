3b9c4bc51214ba26d6e905eb0b8c86823d6d4fc4
Ariel Spencer <arielspencer.tech@gmail.com>
2024-11-05 22:00:39 -0300

    feat: add user score

--- aula01.playground/Pages/pontuacao-usuario.xcplaygroundpage/Contents.swift
+++ aula01.playground/Pages/pontuacao-usuario.xcplaygroundpage/Contents.swift
@@ -1,7 +1,14 @@
-//: [Previous](@previous)
+let pergunta = "Qual o valor de 8 x 2?"
+let respostaCorreta = 16
+let respostaUsuario = 16
+var pontos = 0
 
-import Foundation
+if respostaUsuario == respostaCorreta {
+    pontos += 1
+    print("Parabéns, você acertou!")
+} else {
+    pontos -= 1
+    print("Você errou! A resposta correta é \(respostaCorreta).")
+}
 
-var greeting = "Hello, playground"
-
-//: [Next](@next)
+print("Você fez \(pontos) pontos.")
