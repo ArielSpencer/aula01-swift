e87f873eb97fc4bf7c5101cb0ef3f298ac0059c6
Ariel Spencer <arielspencer.tech@gmail.com>
2024-11-06 22:29:09 -0300

    feat: add optional

---
+++ aula01.playground/Pages/opcionais.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+var telefone: String?
+print(telefone)
+
+telefone = "11 9 9999 9999"
+print(telefone)
+
+var numeroEmString = "45"
+var numero = Int(numeroEmString)
+print(numero)
+
+var numeroEmStringNil = "Ariel"
+var numero2 = Int(numeroEmStringNil)
+print(numero2)
+
+
+
+// Not recommended force -> "!":
+//print(telefone!)
+//
+// e.g. crashing
+//var telefone2: String?
+//print(telefone2!)
+//
