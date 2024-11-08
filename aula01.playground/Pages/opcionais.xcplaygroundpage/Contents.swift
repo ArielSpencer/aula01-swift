8dffeb4344a7bfc25f89ea7e8f5b4d440edf7b50
Ariel Spencer <arielspencer.tech@gmail.com>
2024-11-07 22:41:36 -0300

    feat: add resolution of challenges

--- aula01.playground/Pages/opcionais.xcplaygroundpage/Contents.swift
+++ aula01.playground/Pages/opcionais.xcplaygroundpage/Contents.swift
@@ -66,3 +66,40 @@ if let primeiroCaractere = telefone2?.first {
 // Nil coalescing operator
 telefone2 = nil
 print(telefone2 ?? "Não há valor para telefone 2")
+
+// Challenge 1:
+func verificarPrimo(_ numero: Int) -> Bool {
+    let start = 2
+    for i in start..<numero {
+        if numero % i == 0 {
+            print("\(numero) não é um número primo.")
+            return false
+        }
+    }
+    print("\(numero) é um número primo.")
+    return true
+}
+
+verificarPrimo(17)
+verificarPrimo(10)
+
+// Challenge 2:
+func exibirNome(nome: String?) {
+    guard let nome = nome else {
+        print("Nome não especificado.")
+        return
+    }
+    print("Nome:", nome)
+}
+
+exibirNome(nome: "Ariel")
+exibirNome(nome: nil)
+
+// Challenge 3:
+func dividirConta(_ valor: Double, _ numeroPessoas: Int) -> Double {
+    let valorTotal = valor * 1.1
+    return valorTotal / Double(numeroPessoas)
+}
+
+let valorPorPessoa = dividirConta(532.5, 5)
+print("O valor por pessoa com a taxa do garçom é \(valorPorPessoa)")
