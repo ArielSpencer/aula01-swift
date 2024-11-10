c68d10fd60162357eca248eb9a7917d6dc4ebac3
Ariel Spencer <arielspencer.tech@gmail.com>
2024-11-09 22:51:23 -0300

    feat: add dictionaries

--- aula01.playground/Pages/dicionarios.xcplaygroundpage/Contents.swift
+++ aula01.playground/Pages/dicionarios.xcplaygroundpage/Contents.swift
@@ -1,7 +1,25 @@
-//: [Previous](@previous)
+var pontuacao = [
+    "ArielSpencer": 20,
+    "GiovannaMoeller": 15,
+    "Mari": 25
+]
 
-import Foundation
+// Declaration of an empty dictionary:
+var pessoas: [String: Int] = [:]
+pessoas.reserveCapacity(10)
+//
 
-var greeting = "Hello, playground"
+print(pontuacao["ArielSpencer"])
 
-//: [Next](@next)
+pontuacao.isEmpty
+pontuacao.count
+pontuacao["AnaMaria"] = 27
+print(pontuacao)
+pontuacao["GiovannaMoeller"] = 28
+print(pontuacao)
+pontuacao.updateValue(30, forKey: "Mari")
+pontuacao.updateValue(18, forKey: "Bia")
+print(pontuacao)
+pontuacao["ArielSpencer"] = nil
+pontuacao.removeValue(forKey: "AnaMaria")
+print(pontuacao)
